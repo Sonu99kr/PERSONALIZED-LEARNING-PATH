bb2ffed05f0348749671ef3c34af9d7ae7ae8b55
Sonu99kr <krsonu.work@gmail.com>
2025-08-27 13:45:15 +0530

    Small changes in backend

--- Backend/Middleware/middlewareAuth.js
+++ Backend/Middleware/middlewareAuth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 function authMiddleware(req, res, next) {
-  const token = req.cookies.token;
+  const token = req.cookies.token || req.header("Authorization")?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ error: "Unauthorized, token missing" });
   }
@@ -12,6 +12,9 @@ function authMiddleware(req, res, next) {
     req.user = decoded;
     next();
   } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Token expired" });
+    }
     return res.status(401).json({ error: "Invalid token" });
   }
 }
