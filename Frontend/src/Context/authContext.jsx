2dc7657db897572b7958961ba99f4206b4703686
Sonu99kr <krsonu.work@gmail.com>
2025-08-28 12:10:19 +0530

    small change in frontend authorization

--- Frontend/src/Context/authContext.jsx
+++ Frontend/src/Context/authContext.jsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { set } from "mongoose";
 import React, { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -6,31 +8,55 @@ export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token") || "");
   const [isAuthenticated, setIsAuthenticated] = useState(!!token);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (token) {
-      setIsAuthenticated(true);
-      localStorage.setItem("token", token);
-    } else {
-      setIsAuthenticated(false);
-      localStorage.removeItem("token");
-    }
+    const verifyToken = async () => {
+      if (!token) {
+        setIsAuthenticated(false);
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const res = await axios.get("http://localhost:3002/auth/verify", {
+          headers: { Authorization: `Bearer ${token}` },
+          withCredentials: true,
+        });
+        if (res.data.valid) {
+          setIsAuthenticated(true);
+        }
+      } catch (error) {
+        console.log("Token is Invalid/Expired");
+        setToken("");
+        localStorage.removeItem("token");
+        setIsAuthenticated(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+    verifyToken();
   }, [token]);
 
-  const login = (newToken) => {
+  const isLogin = (newToken) => {
     setToken(newToken);
+    localStorage.setItem("token", newToken);
+    setIsAuthenticated(true);
     navigate("/dashboard");
   };
 
   const logout = () => {
     setToken("");
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
     navigate("/login");
   };
-
   return (
-    <AuthContext.Provider value={{ token, isAuthenticated, login, logout }}>
-      {children}
+    <AuthContext.Provider
+      value={{ token, isAuthenticated, isLogin, logout, loading }}
+    >
+      {!loading && children}
     </AuthContext.Provider>
   );
 };
